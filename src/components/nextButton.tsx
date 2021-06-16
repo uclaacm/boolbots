5e30219b3dba7ecc68ad928ee1e493fa5cfc5d3c
Nathan Endow <35786272+nendow02@users.noreply.github.com>
2021-06-16 15:54:16 -0700

    Lint-fix

--- src/components/nextButton.tsx
+++ src/components/nextButton.tsx
@@ -2,21 +2,21 @@ import '../styles/nextButton.scss';
 import {Link, useLocation} from 'react-router-dom';
 
 function NextButton(): JSX.Element {
-    let end = false;
-    const pages = ["/","/variable","/equal","/comparison","/boolean","/advanced","/output","/python","/experiment"];
-    let nextPage = "/variable";
-    const location = useLocation();
-    const current = location.pathname;
-    if (current === "/experiment") {
-            end = true;
-    } else {
-            nextPage = pages[pages.indexOf(current)+1];
-    }
-    if (end) {
-        return (null as any);
-    } else {
-        return (<Link to={nextPage}  id="nextButton">next</Link>)
-    }
+  let end = false;
+  const pages = ['/','/variable','/equal','/comparison','/boolean','/advanced','/output','/python','/experiment'];
+  let nextPage = '/variable';
+  const location = useLocation();
+  const current = location.pathname;
+  if (current === '/experiment') {
+    end = true;
+  } else {
+    nextPage = pages[pages.indexOf(current)+1];
+  }
+  if (end) {
+    return (null as any);
+  } else {
+    return (<Link to={nextPage}  id="nextButton">next</Link>);
+  }
 }
 
 export default NextButton;
