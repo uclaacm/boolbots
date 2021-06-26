3b0860c02fa2e678a6160f6ee183a31fe7ad831a
Nathan Endow <35786272+nendow02@users.noreply.github.com>
2021-06-25 19:21:02 -0700

    Lint Fix
    Lint fix

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,7 @@
-import LevelSelect from './levelSelect';
-import '../styles/sidebar.scss';
 import { useLocation } from 'react-router-dom';
 import logo from '../assets/logo.svg';
+import LevelSelect from './levelSelect';
+import '../styles/sidebar.scss';
 
 function Sidebar(): JSX.Element {
   const pages = ['/','/variable','/equal','/comparison','/boolean','/advanced','/output','/python','/experiment'];
