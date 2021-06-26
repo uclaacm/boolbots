a64a1566cd6cecbfccb8d725e446848ab5b621b5
Luke <77371718+ldalton02@users.noreply.github.com>
2021-06-25 17:49:32 -0700

    addresses small changes

--- src/components/levelSelect.tsx
+++ src/components/levelSelect.tsx
@@ -10,17 +10,14 @@ function LevelSelect(): JSX.Element {
 
   const [currentPage, setCurrentPage] = useState(6 /* change this to current page once set up */);
 
-  const DropdownArrowUp = <BsArrowUpShort/>;
-  const DropdownArrowDown = <BsArrowDownShort/>;
 
   const createDropDownItems = ():Option[] => {
-    const myArray = pages.map((item) => {
+    return pages.map((item) => {
       return  ({
         label: <div>{item}</div>,
         value: item,
       });
     });
-    return myArray;
   };
 
   const pageArray = createDropDownItems();
@@ -38,8 +35,8 @@ function LevelSelect(): JSX.Element {
         baseClassName="dropdown"
         onChange={onPageChange}
         value={pageArray[currentPage - 1]} // the page the dropdown starts off displaying
-        arrowClosed={DropdownArrowUp}
-        arrowOpen={DropdownArrowDown}
+        arrowClosed={<BsArrowUpShort/>}
+        arrowOpen={<BsArrowDownShort/>}
       />
     </div>
 
