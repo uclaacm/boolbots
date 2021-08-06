a1a02bec58608285263a3ead138330637fc04cc4
Nathan Endow <35786272+nendow02@users.noreply.github.com>
2021-08-06 14:58:14 -0700

    CSS Fix for react-reward
    Better implementation may be needed later on but fixes all css issues that were caused by adding react-rewards

--- src/components/pages/landing.tsx
+++ src/components/pages/landing.tsx
@@ -1,13 +1,13 @@
+import React from 'react';
+import Reward, {RewardElement} from 'react-rewards';
 import robot1 from '../../assets/robots/pg1/robot1pg1.svg';
 import robot2 from '../../assets/robots/pg1/robot2pg1.svg';
 import robot3 from '../../assets/robots/pg1/robot3pg1.svg';
 import robot4 from '../../assets/robots/pg1/robot4pg1.svg';
 import {lineOfCode, CodeFormat}  from  '../shared/codeFormat';
 import {Color} from '../shared/constants';
-import Reward, {RewardElement} from "react-rewards";
-import React from "react";
 
-; //this was a different solution I tried to set ref={rewardRef} but it also didn't work
+//this was a different solution I tried to set ref={rewardRef} but it also didn't work
 
 interface LandingProps {
   onCorrect: () => void;
@@ -27,14 +27,14 @@ function Landing(props:LandingProps): JSX.Element {
     ],
   ];
 
-  const rewardRef = React.useRef<RewardElement>(null)
+  const rewardRef = React.useRef<RewardElement>(null);
 
   function punish() { //I added the this: any to try to fix an error, idk if it works correctly
     rewardRef.current?.punishMe();
   }
 
   /*function getRef(this: any, ref: any) //I tried to make this to replace whats in the {} after ref=
-  { 
+  {
     this.reward = ref;
     return this.reward;
   }*/
@@ -47,20 +47,34 @@ function Landing(props:LandingProps): JSX.Element {
         </div>
         <h1>Select the matching robot:</h1>
         <div className="robots">
-          <Reward ref={rewardRef} type='confetti'>
-            <button className="robot-button" onClick={punish}>
-              <img className="robot-image" src={robot1} alt="green robot, circle head, no mouth, 0 antenna, 2 arms, 3 buttons"></img>
-            </button>
-          </Reward>
-          <button className="robot-button">
-            <img className="robot-image" src={robot2} alt="red robot, circle head, with mouth, 2 antenna, 2 arms, 3 buttons"></img>
-          </button>
-          <button className="robot-button" onClick={props.onCorrect}>
-            <img className="robot-image" src={robot3} alt="green robot, circle head, with mouth, 2 antenna, 2 arms, 3 buttons"></img>
-          </button>
-          <button className="robot-button">
-            <img className="robot-image" src={robot4} alt="blue robot, circle head, with mouth, 0 antenna, 2 arms, 3 buttons"></img>
-          </button>
+          <div>
+            <Reward ref={rewardRef} type='confetti'>
+              <button className="robot-button" onClick={punish}>
+                <img className="robot-image" src={robot1} alt="green robot, circle head, no mouth, 0 antenna, 2 arms, 3 buttons"></img>
+              </button>
+            </Reward>
+          </div>
+          <div>
+            <Reward ref={rewardRef} type='confetti'>
+              <button className="robot-button">
+                <img className="robot-image" src={robot2} alt="red robot, circle head, with mouth, 2 antenna, 2 arms, 3 buttons"></img>
+              </button>
+            </Reward>
+          </div>
+          <div>
+            <Reward ref={rewardRef} type='confetti'>
+              <button className="robot-button" onClick={props.onCorrect}>
+                <img className="robot-image" src={robot3} alt="green robot, circle head, with mouth, 2 antenna, 2 arms, 3 buttons"></img>
+              </button>
+            </Reward>
+          </div>
+          <div>
+            <Reward ref={rewardRef} type='confetti'>
+              <button className="robot-button">
+                <img className="robot-image" src={robot4} alt="blue robot, circle head, with mouth, 0 antenna, 2 arms, 3 buttons"></img>
+              </button>
+            </Reward>
+          </div>
         </div>
       </div>
     </div>
